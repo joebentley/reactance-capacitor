22c2620150289c847f9cbf49170157755f7cb29d
Joe Bentley <joebentley10@gmail.com>
2016-09-01 15:06:01 +0100

    Only show positive values

--- js/lib.js
+++ js/lib.js
@@ -5,17 +5,19 @@ var lib = {}
 ;(function (ns) {
   ns.runApp = function (graphElemID) {
     var board = JXG.JSXGraph.initBoard(graphElemID, {
-      boundingBox: [-1, 5, 5, -1],
-      axis: true,
+      boundingbox: [-0.5, 10, 10, -0.5],
+      keepaspectratio: false,
       showCopyright: false
     })
 
-    var frequency = board.create('slider', [[1, 1], [3, 1], [0.01, 1, 5]],
+    var frequency = board.create('slider', [[3, 1], [6, 1], [0.01, 1, 5]],
       { name: 'frequency in Hz' })
 
     board.create('functiongraph', [
       function (C) {
-        return 1 / (2 * Math.PI * frequency.Value() * C)
+        if (C > 0) {
+          return 1 / (2 * Math.PI * frequency.Value() * C)
+        }
       }
     ])
 
@@ -33,7 +35,7 @@ var lib = {}
       withLabel: true,
       label: {
         position: 'rt',  // possible values are 'lft', 'rt', 'top', 'bot'
-        offset: [-30, 0]   // (in pixels)
+        offset: [30, 0]   // (in pixels)
       }
     })
   }
